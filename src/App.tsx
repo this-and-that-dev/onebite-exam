f7dfd5c1ccee9427f8536be847c6ff242ebff391
공수한 <ksh92315@pionnet.co.kr>
2026-01-05 23:03:20 +0900

    8. (2.2) Tailwind CSS 핵심정리 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="text-2xl font-bold underline">Hello World!</div>
+      {/*1. 타이포 그래프*/}
+      <div className="bg-auto text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
+      <div className="text-lg font-bold">text-lg</div>
+      <div className="text-xl font-extrabold">text-xl</div>
+      <div className="text-2xl font-black">text-2xl</div>
+      <div className="text-[13px]">text-13px</div>
+      {/*2. 백그라운드 컬러*/}
+      <div className="bg-amber-500">amber-500</div>
+      {/*3. 사이즈*/}
+      <div className="h-10 w-20 bg-blue-500">box</div>
+      <div className="w-[20px] bg-green-500">box</div>
+      <div className="h-20 w-full bg-yellow-500">box</div>
+      {/*4. 여백*/}
+      <div className="m-5 ml-10 h-50 w-50 bg-red-400 px-5 py-5">
+        <div className="h-full w-full bg-black"></div>
+      </div>
     </>
   );
 }
