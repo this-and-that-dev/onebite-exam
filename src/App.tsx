847be12734e8e020dd1bcd5568f9267683c79ea1
공수한 <ksh92315@pionnet.co.kr>
2026-01-11 20:12:11 +0900

    11. (2.4) Shadcn/ui를 소개합니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
 import "./App.css";
+import { Button } from "@/components/ui/button.tsx";
+import { cn } from "@/lib/utils.ts";
 
 function App() {
+  const isActive = false;
+
   return (
-    <>
-      {/*1. 타이포 그래프*/}
-      <div className="bg-auto text-xs text-red-500">text-xs</div>
-      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
-      <div className="text-lg font-bold">text-lg</div>
-      <div className="text-xl font-extrabold">text-xl</div>
-      <div className="text-2xl font-black">text-2xl</div>
-      <div className="text-[13px]">text-13px</div>
-      {/*2. 백그라운드 컬러*/}
-      <div className="bg-amber-500">amber-500</div>
-      {/*3. 사이즈*/}
-      <div className="h-10 w-20 bg-blue-500">box</div>
-      <div className="w-[20px] bg-green-500">box</div>
-      <div className="h-20 w-full bg-yellow-500">box</div>
-      {/*4. 여백*/}
-      <div className="m-5 ml-10 h-50 w-50 bg-red-400 px-5 py-5">
-        <div className="h-full w-full bg-black"></div>
+    <div>
+      <Button>버튼!</Button>
+      <div
+        className={cn(
+          "w-10 text-sm",
+          isActive ? "text-green-500" : "text-red-500",
+        )}
+      >
+        isActive
       </div>
-      {/*5. 보더*/}
-      <div className="m-5 rounded-md border-2 border-red-500">border</div>
-      {/*6. 플렉스 컨테이너*/}
-      <div className="flex flex-row items-center-safe justify-evenly">
-        <div className="h-10 w-10 border">1</div>
-        <div className="h-20 w-10 flex-3 border">2</div>
-        <div className="h-30 w-10 flex-2 border">3</div>
-        <div className="h-40 w-10 border">4</div>
+      <div
+        className={
+          "w-10 text-sm " + (isActive ? "text-green-500" : "text-red-500")
+        }
+      >
+        isActive
       </div>
-    </>
+      <div className="text-primary">Primary</div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
+    </div>
   );
 }
 
