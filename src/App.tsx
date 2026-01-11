8e2b0688e4809eae801443e1d7f91fbe5bab84e4
공수한 <ksh92315@pionnet.co.kr>
2026-01-11 21:15:29 +0900

    14. (2.7) React Router V7 핵심정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,136 +1,27 @@
 import "./App.css";
-import { Button } from "@/components/ui/button.tsx";
-import { cn } from "@/lib/utils.ts";
-import { Input } from "@/components/ui/input.tsx";
-import { Textarea } from "@/components/ui/textarea.tsx";
-import { Toaster } from "@/components/ui/sonner.tsx";
-import { toast } from "sonner";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel.tsx";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover.tsx";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog.tsx";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog.tsx";
+import { Outlet, Route, Routes } from "react-router";
+import IndexPage from "@/pages/index-page.tsx";
+import SignInPage from "@/pages/sign-in-page.tsx";
+import SignUpPage from "@/pages/sign-up-page.tsx";
 
-import { Amphora } from "lucide-react";
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet /> {/*하위 컴포넌트들의 위치를 지정한다*/}
+    </div>
+  );
+}
 
 function App() {
-  const isActive = false;
-
   return (
-    <div className="p-5">
-      <Amphora />
-      <AlertDialog>
-        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogTitle>Title</AlertDialogTitle>
-          <AlertDialogDescription>
-            <div>body</div>
-            <div>
-              <AlertDialogCancel
-                onClick={() => {
-                  console.log("cancel");
-                }}
-              >
-                Cancel
-              </AlertDialogCancel>
-              <AlertDialogAction
-                onClick={() => {
-                  console.log("action");
-                }}
-              >
-                Action
-              </AlertDialogAction>
-            </div>
-          </AlertDialogDescription>
-        </AlertDialogContent>
-      </AlertDialog>
-
-      <Dialog>
-        <DialogTrigger>Open Dialog</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Title</DialogTitle>
-            <DialogDescription>Description</DialogDescription>
-          </DialogHeader>
-          <div>Body</div>
-        </DialogContent>
-      </Dialog>
-
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button>Open</Button>
-        </PopoverTrigger>
-        <PopoverContent>Content!</PopoverContent>
-      </Popover>
-
-      <Carousel className="mx-10">
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">1</CarouselItem>
-          <CarouselItem className="basis-1/3">2</CarouselItem>
-          <CarouselItem className="basis-1/3">3</CarouselItem>
-          <CarouselItem className="basis-1/3">4</CarouselItem>
-          <CarouselItem className="basis-1/3">5</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-
-      <Toaster />
-      <Textarea />
-
-      <Input placeholder="입력..." />
-
-      <Button
-        onClick={() => {
-          toast("토스트 메시지", {
-            position: "top-center",
-          });
-        }}
-      >
-        버튼!
-      </Button>
-      <Button variant="destructive">버튼!</Button>
-      <Button variant="ghost">버튼!</Button>
-      <Button variant="link">버튼!</Button>
-      <Button variant="outline">버튼!</Button>
-      <Button variant="secondary">버튼!</Button>
-
-      <div
-        className={cn(
-          "w-10 text-sm",
-          isActive ? "text-green-500" : "text-red-500",
-        )}
-      >
-        isActive
-      </div>
-      <div className="text-primary">Primary</div>
-      <div className="text-muted">Muted</div>
-      <div className="text-destructive">Destructive</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
