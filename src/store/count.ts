6ee1f5d51fcf86bbd577a48d781a4bbbe845fad8
공수한 <ksh92315@pionnet.co.kr>
2026-01-12 23:06:01 +0900

    18. (3.4) Zustand 미들웨어 1

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,29 +1,58 @@
 import { create } from "zustand";
 //create 는 state, action 함수를 포함하는 객체인 store 를 생성한다.
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+import { combine } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      set((store) => ({
-        count: store.count + 1,
-      }));
-    },
-    decrease: () => {
-      set((store) => ({
-        count: store.count - 1,
-      }));
-    },
-  },
-}));
+//combine middleware
+//immer middleware : return 없이 state 를 바꿀수 있다.
+export const useCountStore = create(
+  immer(
+    combine({ count: 0 }, (set, get) => ({
+      actions: {
+        increase: () => {
+          // set((state) => ({
+          //   count: state.count + 1,
+          // }));
+          set((state) => {
+            state.count += 1;
+          });
+        },
+        decrease: () => {
+          // set((store) => ({
+          //   count: store.count - 1,
+          // }));
+          set((state) => {
+            state.count -= 1;
+          });
+        },
+      },
+    })),
+  ),
+);
+// type Store = {
+//   count: number;
+//   actions: {
+//     increase: () => void;
+//     decrease: () => void;
+//   };
+// };
+
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increase: () => {
+//       set((state) => ({
+//         count: state.count + 1,
+//       }));
+//     },
+//     decrease: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
 
 export const useCount = () => {
   return useCountStore((store) => store.count);
