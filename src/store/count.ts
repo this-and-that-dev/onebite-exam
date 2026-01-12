612e073b339d3849b1604d4cfea1b534e2597844
공수한 <ksh92315@pionnet.co.kr>
2026-01-12 23:36:06 +0900

    19. (3.5) Zustand 미들웨어 2

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,35 +1,72 @@
 import { create } from "zustand";
 //create 는 state, action 함수를 포함하는 객체인 store 를 생성한다.
 
-import { combine } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
-//combine middleware
+//combine middleware : type 선언 같은 불필요한 짓 안해도됨
 //immer middleware : return 없이 state 를 바꿀수 있다.
+//devtools -> persist -> subscribeWithSelector -> immer -> combine 순서대로 하는게 버그 발생확률이 없다.
 export const useCountStore = create(
-  immer(
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increase: () => {
-          // set((state) => ({
-          //   count: state.count + 1,
-          // }));
-          set((state) => {
-            state.count += 1;
-          });
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                // set((state) => ({
+                //   count: state.count + 1,
+                // }));
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                // set((store) => ({
+                //   count: store.count - 1,
+                // }));
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore", //로컬스토리지 또는 세션 스토리지에 저장할 키
+        //count만 로컬스토리지 또는 세션스토리지에 저장한다.
+        partialize: (store) => {
+          return {
+            count: store.count,
+          };
         },
-        decrease: () => {
-          // set((store) => ({
-          //   count: store.count - 1,
-          // }));
-          set((state) => {
-            state.count -= 1;
-          });
-        },
+        storage: createJSONStorage(() => {
+          return sessionStorage;
+        }), //세션 스토리지 저장
       },
-    })),
+    ),
+    {
+      name: "countStore",
+    },
   ),
 );
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Listener
+    console.log(count, prevCount);
+  },
+);
+
 // type Store = {
 //   count: number;
 //   actions: {
