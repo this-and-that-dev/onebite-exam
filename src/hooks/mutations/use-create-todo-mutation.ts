6176a7c80541a3273269cb6f22f15a76dafc49d7
공수한 <ksh92315@pionnet.co.kr>
2026-01-19 21:55:14 +0900

    29. (4.8) 캐시 데이터 다루기 2 - 응답 결과값 이용하기

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo } from "@/api/create-todo.ts";
 import { QUERY_KEYS } from "@/lib/constants.ts";
+import type { Todo } from "@/types.ts";
 
 export function useCreateTodoMutation() {
   const queryClient = useQueryClient(); //useQuery 를 사용하는 모든 데이터를 보관하는 저장소
@@ -12,11 +13,18 @@ export function useCreateTodoMutation() {
     //요청이 종료됐을 때 이벤트
     onSettled: () => {},
     //요청이 성공했을 때 이벤트
-    onSuccess: async () => {
-      //캐시 무효화
-      await queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+    //매개변수로 createTodo 의 반환값이 들어온다.
+    onSuccess: async (newTodo) => {
+      //prevTodos 는 키값에 저장된 데이터가 들어있다.
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
+
+      //캐시 무효화
+      // await queryClient.invalidateQueries({
+      //   queryKey: QUERY_KEYS.todo.list,
+      // });
     },
     //요청이 에러났을 때 이벤트
     onError: (error) => {
