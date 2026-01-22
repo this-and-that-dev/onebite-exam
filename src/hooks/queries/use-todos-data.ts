52c260c5c42b03ba763c0a797c82cee6730d961f
공수한 <ksh92315@pionnet.co.kr>
2026-01-22 23:44:19 +0900

    33. (4.12) 캐시 정규화하기 1

--- src/hooks/queries/use-todos-data.ts
+++ src/hooks/queries/use-todos-data.ts
@@ -1,10 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { fetchTodos } from "@/api/fetch-todos.ts";
 import { QUERY_KEYS } from "@/lib/constants.ts";
+import type { Todo } from "@/types.ts";
 
 export function useTodosData() {
+  const queryClient = useQueryClient();
+
   return useQuery({
-    queryFn: fetchTodos,
+    // queryFn: fetchTodos,
+    queryFn: async () => {
+      const todos = await fetchTodos();
+
+      todos.forEach((todo) => {
+        queryClient.setQueryData<Todo>(QUERY_KEYS.todo.detail(todo.id), todo);
+      });
+
+      return todos.map((todo) => todo.id);
+    },
     queryKey: QUERY_KEYS.todo.list,
   });
 }
